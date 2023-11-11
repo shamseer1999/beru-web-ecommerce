8b16db9f60e9d293f99fc50688be72ca0615855f
Shamseer-tt <shamseer.primalcodes@gmail.com>
2023-11-11 10:08:58 +0530

    wishlist setup

--- resources/views/users/wishlist.blade.php
+++ resources/views/users/wishlist.blade.php
@@ -14,10 +14,56 @@
                       <img src="{{ asset('storage/products/'.$val->image)}}" alt="" style="width:100%;object-fit:cover;">
                     </div>
                   </div>
+                  <div class="card-body">
+                    <div class="text-right buttons">
+                        @if ($val->product_stock > 0)
+                            <button class="btn btn-outline-dark" id="wishlist-btn" onclick="addToWishlist({{$val->id}})" data-id="{{$val->id}}">add to wishlist</button>
+                            <button class="btn btn-dark" onclick="addToCart({{$val->id}})" data-id="{{$val->id}}">Add to cart</button>
+                        @else
+                            <label for="" style="color: rgb(241, 154, 154)">Currently unavailable</label>
+                        @endif
+
+                    </div>
+                </div>
                 @endforeach
             @endif
         </div>
       </div>
     @endforeach
 @endif
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+<script>
+    function addToCart(vl){
+    var dataId = vl
+    if(dataId){
+        $.ajax({
+            headers:{
+                'X-CSRF-TOKEN':$('meta[name = "csrf-token"]').attr('content')
+            },
+            url:"{{url('/add-to-cart')}}",
+            type:'POST',
+            data:{
+                id:dataId
+            },
+            success:function(response){
+                console.log(response)
+                if(response.message == 'success'){
+                    alert('Item added to cart successfully')
+                    location.reload()
+                }else if(response.message == 'not_authenticated'){
+                    console.log($('#auth-btn'));
+                    $("#sign-in-form").hide()
+                    $("#login-form").show()
+                    $('#auth-btn').click();
+                }else if(response.message == 'not_exist'){
+                    alert("Item doesn't exist")
+                }else{
+                    alert(response.message)
+                }
+            }
+        })
+    }
+
+}
+</script>
 @endsection
